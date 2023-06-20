b2a77235f0f08edd4613fc421cb1d7c1aa9d751c
SKhai77 <orionkhai@gmail.com>
2023-06-20 12:48:05 -0500

    Added starter code

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+  //1. Need to prompt te user for the passwrod criteria
+  var userInput = window.prompt("How many characters would you like your password to contain?");
+
+  if (!userInput) {
+    return;
+  }
+
+  var userInput = userInput.window(" ");
+  }
+
+  // a. Set a passworld lenght from 8 - 128.
+  var minCharsLenght = "8";
+  var maxCharsLenght = "128";
+
+  window.confirm("Click OK to confirm including Special characters.");
+
+  window.confirm("Click OK to confirm including Numeric characters.");
+
+  window.confirm("Click OK to confirm including Lowercase characters.");
+  
+  window.confirm("Click OK to confirm including Uppercase characters.");
+
+  // b. 
+  var charTypes = {
+    charLowerCase: "abcdefghijklmnopqrstuvwxyz",
+    charUpperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    charNumbers: "0123456789",
+    charSymbols: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+  }
+  //2. Needed Input validation
+  // 3. Need to able to generate the password based on the criteria
+
+  // 4. Return the value
+
+  return password;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +50,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
